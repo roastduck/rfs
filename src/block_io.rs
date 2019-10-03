809dc8b30dba14baf4ecef7c70c13984f1ee04dc
roastduck <beantang.tang@gmail.com>
2019-10-02 20:04:45 -0700

    eliminated some warnings

--- src/block_io.rs
+++ src/block_io.rs
@@ -2,8 +2,8 @@ pub const BLOCK_SIZE: usize = 4096;
 pub type Id = u16;
 
 pub trait BlockIO {
-    fn read(&mut self, blockId: Id) -> Result<[u8; BLOCK_SIZE], std::io::Error>;
-    fn write(&mut self, blockId: Id, data: &[u8]) -> Result<(), std::io::Error>;
+    fn read(&mut self, block_id: Id) -> Result<[u8; BLOCK_SIZE], std::io::Error>;
+    fn write(&mut self, block_id: Id, data: &[u8]) -> Result<(), std::io::Error>;
 }
 
 pub struct FakeMemBlockIO {
@@ -11,8 +11,8 @@ pub struct FakeMemBlockIO {
 }
 
 impl FakeMemBlockIO {
-    fn ensure_length(&mut self, blockId: Id) {
-        while self.blocks.len() <= blockId as usize {
+    fn ensure_length(&mut self, block_id: Id) {
+        while self.blocks.len() <= block_id as usize {
             self.blocks.push(Box::new([0; BLOCK_SIZE]));
         }
     }
@@ -23,15 +23,15 @@ impl FakeMemBlockIO {
 }
 
 impl BlockIO for FakeMemBlockIO {
-    fn read(&mut self, blockId: Id) -> Result<[u8; BLOCK_SIZE], std::io::Error> {
-        self.ensure_length(blockId);
-        Ok(*self.blocks[blockId as usize])
+    fn read(&mut self, block_id: Id) -> Result<[u8; BLOCK_SIZE], std::io::Error> {
+        self.ensure_length(block_id);
+        Ok(*self.blocks[block_id as usize])
     }
 
-    fn write(&mut self, blockId: Id, data: &[u8]) -> Result<(), std::io::Error> {
+    fn write(&mut self, block_id: Id, data: &[u8]) -> Result<(), std::io::Error> {
         assert_eq!(data.len(), BLOCK_SIZE);
-        self.ensure_length(blockId);
-        self.blocks[blockId as usize].copy_from_slice(data);
+        self.ensure_length(block_id);
+        self.blocks[block_id as usize].copy_from_slice(data);
         Ok(())
     }
 }
--- src/inode.rs
+++ src/inode.rs
@@ -17,35 +17,35 @@ pub struct Inode {
     data: [u8; BLOCK_SIZE],
 }
 
-const generation_off: usize = 0;
-const generation_size: usize = std::mem::size_of::<u64>();
+const GENERATION_OFF: usize = 0;
+const GENERATION_SIZE: usize = std::mem::size_of::<u64>();
 
-const length_off: usize = generation_off + generation_size;
-const length_size: usize = std::mem::size_of::<u32>();
+const LENGTH_OFF: usize = GENERATION_OFF + GENERATION_SIZE;
+const LENGTH_SIZE: usize = std::mem::size_of::<u32>();
 
-const atime_off: usize = length_off + length_size;
-const atime_size: usize = std::mem::size_of::<i64>() + std::mem::size_of::<i32>(); // sec + nsec
+const ATIME_OFF: usize = LENGTH_OFF + LENGTH_SIZE;
+const ATIME_SIZE: usize = std::mem::size_of::<i64>() + std::mem::size_of::<i32>(); // sec + nsec
 
-const mtime_off: usize = atime_off + atime_size;
-const mtime_size: usize = std::mem::size_of::<i64>() + std::mem::size_of::<i32>(); // sec + nsec
+const MTIME_OFF: usize = ATIME_OFF + ATIME_SIZE;
+const MTIME_SIZE: usize = std::mem::size_of::<i64>() + std::mem::size_of::<i32>(); // sec + nsec
 
-const ctime_off: usize = mtime_off + mtime_size;
-const ctime_size: usize = std::mem::size_of::<i64>() + std::mem::size_of::<i32>(); // sec + nsec
+const CTIME_OFF: usize = MTIME_OFF + MTIME_SIZE;
+const CTIME_SIZE: usize = std::mem::size_of::<i64>() + std::mem::size_of::<i32>(); // sec + nsec
 
-const type_perm_off: usize = ctime_off + ctime_size;
-const type_perm_size: usize = std::mem::size_of::<u16>();
+const TYPE_PERM_OFF: usize = CTIME_OFF + CTIME_SIZE;
+const TYPE_PERM_SIZE: usize = std::mem::size_of::<u16>();
 
-const nlink_off: usize = type_perm_off + type_perm_size;
-const nlink_size: usize = std::mem::size_of::<u16>();
+const NLINK_OFF: usize = TYPE_PERM_OFF + TYPE_PERM_SIZE;
+const NLINK_SIZE: usize = std::mem::size_of::<u16>();
 
-const uid_off: usize = nlink_off + nlink_size;
-const uid_size: usize = std::mem::size_of::<u32>();
+const UID_OFF: usize = NLINK_OFF + NLINK_SIZE;
+const UID_SIZE: usize = std::mem::size_of::<u32>();
 
-const gid_off: usize = uid_off + uid_size;
-const gid_size: usize = std::mem::size_of::<u32>();
+const GID_OFF: usize = UID_OFF + UID_SIZE;
+const GID_SIZE: usize = std::mem::size_of::<u32>();
 
-const index_off: usize = gid_off + gid_size;
-const index_size: usize = std::mem::size_of::<Id>();
+const INDEX_OFF: usize = GID_OFF + GID_SIZE;
+const INDEX_SIZE: usize = std::mem::size_of::<Id>();
 
 /// For layout of each inode is like:
 /// [ generation (8B) | length (4B) | last access time (12B) | last modification time (12B) |
@@ -70,58 +70,58 @@ impl Inode {
     }
 
     pub fn generation(&self) -> u64 {
-        u64::from_le_bytes(self.data[generation_off .. generation_off + generation_size].try_into().unwrap())
+        u64::from_le_bytes(self.data[GENERATION_OFF .. GENERATION_OFF + GENERATION_SIZE].try_into().unwrap())
     }
 
     // No need to set geneartion
 
     pub fn length(&self) -> u32 {
-        u32::from_le_bytes(self.data[length_off .. length_off + length_size].try_into().unwrap())
+        u32::from_le_bytes(self.data[LENGTH_OFF .. LENGTH_OFF + LENGTH_SIZE].try_into().unwrap())
     }
 
     pub fn set_length(&mut self, length: u32) {
-        self.data[length_off .. length_off + length_size].copy_from_slice(&length.to_le_bytes());
+        self.data[LENGTH_OFF .. LENGTH_OFF + LENGTH_SIZE].copy_from_slice(&length.to_le_bytes());
         self.dirty = true;
     }
 
     pub fn atime(&self) -> time::Timespec {
-        let sec = i64::from_le_bytes(self.data[atime_off .. atime_off + 8].try_into().unwrap());
-        let nsec = i32::from_le_bytes(self.data[atime_off + 8 .. atime_off + 12].try_into().unwrap());
+        let sec = i64::from_le_bytes(self.data[ATIME_OFF .. ATIME_OFF + 8].try_into().unwrap());
+        let nsec = i32::from_le_bytes(self.data[ATIME_OFF + 8 .. ATIME_OFF + 12].try_into().unwrap());
         time::Timespec { sec: sec, nsec: nsec }
     }
 
     pub fn set_atime(&mut self, atime: time::Timespec) {
-        self.data[atime_off .. atime_off + 8].copy_from_slice(&atime.sec.to_le_bytes());
-        self.data[atime_off + 8 .. atime_off + 12].copy_from_slice(&atime.nsec.to_le_bytes());
+        self.data[ATIME_OFF .. ATIME_OFF + 8].copy_from_slice(&atime.sec.to_le_bytes());
+        self.data[ATIME_OFF + 8 .. ATIME_OFF + 12].copy_from_slice(&atime.nsec.to_le_bytes());
         self.dirty = true;
     }
 
     pub fn mtime(&self) -> time::Timespec {
-        let sec = i64::from_le_bytes(self.data[mtime_off .. mtime_off + 8].try_into().unwrap());
-        let nsec = i32::from_le_bytes(self.data[mtime_off + 8 .. mtime_off + 12].try_into().unwrap());
+        let sec = i64::from_le_bytes(self.data[MTIME_OFF .. MTIME_OFF + 8].try_into().unwrap());
+        let nsec = i32::from_le_bytes(self.data[MTIME_OFF + 8 .. MTIME_OFF + 12].try_into().unwrap());
         time::Timespec { sec: sec, nsec: nsec }
     }
 
     pub fn set_mtime(&mut self, mtime: time::Timespec) {
-        self.data[mtime_off .. mtime_off + 8].copy_from_slice(&mtime.sec.to_le_bytes());
-        self.data[mtime_off + 8 .. mtime_off + 12].copy_from_slice(&mtime.nsec.to_le_bytes());
+        self.data[MTIME_OFF .. MTIME_OFF + 8].copy_from_slice(&mtime.sec.to_le_bytes());
+        self.data[MTIME_OFF + 8 .. MTIME_OFF + 12].copy_from_slice(&mtime.nsec.to_le_bytes());
         self.dirty = true;
     }
 
     pub fn ctime(&self) -> time::Timespec {
-        let sec = i64::from_le_bytes(self.data[ctime_off .. ctime_off + 8].try_into().unwrap());
-        let nsec = i32::from_le_bytes(self.data[ctime_off + 8 .. ctime_off + 12].try_into().unwrap());
+        let sec = i64::from_le_bytes(self.data[CTIME_OFF .. CTIME_OFF + 8].try_into().unwrap());
+        let nsec = i32::from_le_bytes(self.data[CTIME_OFF + 8 .. CTIME_OFF + 12].try_into().unwrap());
         time::Timespec { sec: sec, nsec: nsec }
     }
 
     pub fn set_ctime(&mut self, ctime: time::Timespec) {
-        self.data[ctime_off .. ctime_off + 8].copy_from_slice(&ctime.sec.to_le_bytes());
-        self.data[ctime_off + 8 .. ctime_off + 12].copy_from_slice(&ctime.nsec.to_le_bytes());
+        self.data[CTIME_OFF .. CTIME_OFF + 8].copy_from_slice(&ctime.sec.to_le_bytes());
+        self.data[CTIME_OFF + 8 .. CTIME_OFF + 12].copy_from_slice(&ctime.nsec.to_le_bytes());
         self.dirty = true;
     }
 
     pub fn kind(&self) -> Result<fuse::FileType, std::io::Error> {
-        let type_perm = u16::from_le_bytes(self.data[type_perm_off .. type_perm_off + type_perm_size].try_into().unwrap());
+        let type_perm = u16::from_le_bytes(self.data[TYPE_PERM_OFF .. TYPE_PERM_OFF + TYPE_PERM_SIZE].try_into().unwrap());
         match type_perm >> 12 {
             0 => Ok(fuse::FileType::RegularFile),
             1 => Ok(fuse::FileType::Directory),
@@ -131,67 +131,67 @@ impl Inode {
     }
 
     pub fn set_kind(&mut self, kind: fuse::FileType) -> Result<(), std::io::Error> {
-        let mut type_perm = u16::from_le_bytes(self.data[type_perm_off .. type_perm_off + type_perm_size].try_into().unwrap());
+        let mut type_perm = u16::from_le_bytes(self.data[TYPE_PERM_OFF .. TYPE_PERM_OFF + TYPE_PERM_SIZE].try_into().unwrap());
         type_perm = (type_perm & 0x0fff) | (match kind {
             fuse::FileType::RegularFile => 0,
             fuse::FileType::Directory => 1,
             fuse::FileType::Symlink => 2,
             _ => return Err(std::io::Error::from_raw_os_error(libc::EINVAL))
         } << 12);
-        self.data[type_perm_off .. type_perm_off + type_perm_size].copy_from_slice(&type_perm.to_le_bytes());
+        self.data[TYPE_PERM_OFF .. TYPE_PERM_OFF + TYPE_PERM_SIZE].copy_from_slice(&type_perm.to_le_bytes());
         self.dirty = true;
         Ok(())
     }
 
     pub fn perm(&self) -> u16 {
-        let type_perm = u16::from_le_bytes(self.data[type_perm_off .. type_perm_off + type_perm_size].try_into().unwrap());
+        let type_perm = u16::from_le_bytes(self.data[TYPE_PERM_OFF .. TYPE_PERM_OFF + TYPE_PERM_SIZE].try_into().unwrap());
         type_perm & 0x0fff
     }
 
     pub fn set_perm(&mut self, perm: u16) {
-        let mut type_perm = u16::from_le_bytes(self.data[type_perm_off .. type_perm_off + type_perm_size].try_into().unwrap());
+        let mut type_perm = u16::from_le_bytes(self.data[TYPE_PERM_OFF .. TYPE_PERM_OFF + TYPE_PERM_SIZE].try_into().unwrap());
         type_perm = (type_perm & 0xf000) | perm;
-        self.data[type_perm_off .. type_perm_off + type_perm_size].copy_from_slice(&type_perm.to_le_bytes());
+        self.data[TYPE_PERM_OFF .. TYPE_PERM_OFF + TYPE_PERM_SIZE].copy_from_slice(&type_perm.to_le_bytes());
         self.dirty = true;
     }
 
     pub fn nlink(&self) -> u16 {
-        u16::from_le_bytes(self.data[nlink_off .. nlink_off + nlink_size].try_into().unwrap())
+        u16::from_le_bytes(self.data[NLINK_OFF .. NLINK_OFF + NLINK_SIZE].try_into().unwrap())
     }
 
     pub fn set_nlink(&mut self, nlink: u16) {
-        self.data[nlink_off .. nlink_off + nlink_size].copy_from_slice(&nlink.to_le_bytes());
+        self.data[NLINK_OFF .. NLINK_OFF + NLINK_SIZE].copy_from_slice(&nlink.to_le_bytes());
         self.dirty = true;
     }
 
     pub fn uid(&self) -> u32 {
-        u32::from_le_bytes(self.data[uid_off .. uid_off + uid_size].try_into().unwrap())
+        u32::from_le_bytes(self.data[UID_OFF .. UID_OFF + UID_SIZE].try_into().unwrap())
     }
 
     pub fn set_uid(&mut self, uid: u32) {
-        self.data[uid_off .. uid_off + uid_size].copy_from_slice(&uid.to_le_bytes());
+        self.data[UID_OFF .. UID_OFF + UID_SIZE].copy_from_slice(&uid.to_le_bytes());
         self.dirty = true;
     }
 
     pub fn gid(&self) -> u32 {
-        u32::from_le_bytes(self.data[gid_off .. gid_off + gid_size].try_into().unwrap())
+        u32::from_le_bytes(self.data[GID_OFF .. GID_OFF + GID_SIZE].try_into().unwrap())
     }
 
     pub fn set_gid(&mut self, gid: u32) {
-        self.data[gid_off .. gid_off + gid_size].copy_from_slice(&gid.to_le_bytes());
+        self.data[GID_OFF .. GID_OFF + GID_SIZE].copy_from_slice(&gid.to_le_bytes());
         self.dirty = true;
     }
 
     pub fn data_block(&self, index: usize) -> Id {
         Id::from_le_bytes(
-            self.data[index_off + index * index_size .. index_off + (index + 1) * index_size]
+            self.data[INDEX_OFF + index * INDEX_SIZE .. INDEX_OFF + (index + 1) * INDEX_SIZE]
             .try_into().unwrap()
         )
     }
 
     /// Set data block pointer. Need to flush manually later
     pub fn set_data_block(&mut self, index: usize, data_block: Id) {
-        self.data[index_off + index * index_size .. index_off + (index + 1) * index_size]
+        self.data[INDEX_OFF + index * INDEX_SIZE .. INDEX_OFF + (index + 1) * INDEX_SIZE]
             .copy_from_slice(&data_block.to_le_bytes());
         self.dirty = true;
     }
